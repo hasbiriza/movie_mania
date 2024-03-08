e7c3796e9b5aca606982355cb627864ba8b89e1a
hasbiriza <hasbiriza@gmail.com>
2024-03-08 22:04:13 +0700

    Commit1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
 
 function App() {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  // Loading Pertama Kali
+  useEffect(() => {
+    getMovieList().then((result) => {
+      console.log(result);
+      setPopularMovies(result);
+    });
+  }, []);
+
+
+// Per 1  Card Popular Movies
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img
+            className="Movie-image"
+            alt="Poster Movie"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+          />
+          <div className="Movie-date">Release : {movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
+
+
+
+  // Search Movie
+  const search = async(q) => {
+    const query = await searchMovie (q)
+    setPopularMovies(query.results)
+    console.log({query : query});
+  };
+
+  // BENTUKAN PAGENYA //
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hasbi Movie Mania Jos!</h1>
+        <input
+          onChange={({ target }) => search(target.value)}
+          placeholder="Film Kesayanganmu?"
+          className="Movie-search"
+        />
+        <div className="Movie-container">
+        <PopularMovieList/>
+        </div>
       </header>
     </div>
   );
---
+++ src/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const apiKey = process.env.REACT_APP_APIKEY
+const baseURL = process.env.REACT_APP_BASEURL
+
+
+
+export const getMovieList = async() => {
+    const movie = await axios.get(
+        `${baseURL}/movie/popular?page=1&api_key=${process.env.REACT_APP_APIKEY}`
+        )
+    return movie.data.results
+}
+
+
+
+export const searchMovie = async (q) => {
+    const search = await axios.get(`${baseURL}/search/movie?query=${q}&api_key=${apiKey}`)
+    return search.data
+}
